d7d7985f64e72556e0b99d640af991fa1a8d6b84
wildlifehexagon <crossthebreeze@gmail.com>
2018-04-10 10:24:23 -0500

    add absolute path

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,10 +2,10 @@ import React from "react"
 import { BrowserRouter as Router } from "react-router-dom"
 import { Header, Footer } from "dicty-components-header-footer"
 import { Navbar } from "dicty-components-navbar"
-import { FooterLinks } from "./constants/Footer"
-import { NavbarLinks } from "./constants/Navbar"
-import Routes from "./Routes"
-import { headerItems, generateLinks } from "./utils/headerItems"
+import { FooterLinks } from "constants/Footer"
+import { NavbarLinks } from "constants/Navbar"
+import Routes from "../Routes"
+import { headerItems, generateLinks } from "utils/headerItems"
 
 const App = () => {
   return (
