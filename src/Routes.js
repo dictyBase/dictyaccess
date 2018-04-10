4f58313fcabac0653a5aa30c13419c56418d5e1e
wildlifehexagon <crossthebreeze@gmail.com>
2018-04-10 10:28:20 -0500

    update routing

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,23 @@
 // @flow
 import React from "react"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom"
 import Dashboard from "./components/Dashboard"
 
 const Routes = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Dashboard} />
+        <Route
+          exact
+          path="/"
+          component={Dashboard => <Redirect to="/dashboard" />}
+        />
+        <Route path="/dashboard" component={Dashboard} />
       </Switch>
     </Router>
   )
