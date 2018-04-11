f55c52f9e0931f8c1c93a602d25cff8bfede2567
wildlifehexagon <crossthebreeze@gmail.com>
2018-04-11 14:50:40 -0500

    add typeface-roboto

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom"
 import "./index.css"
 import App from "app/layout/App"
 import registerServiceWorker from "./registerServiceWorker"
+import "typeface-roboto"
 
 ReactDOM.render(<App />, document.getElementById("root"))
 registerServiceWorker()
