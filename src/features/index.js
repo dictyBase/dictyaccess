b2127849a788bc2714755be7375a07f81f4fd2d5
wildlifehexagon <crossthebreeze@gmail.com>
2018-04-12 12:34:32 -0500

    remove unneeded file

--- src/features/index.js
+++
@@ -1,4 +0,0 @@
-// this file compiles all component exports into one location for ease of use
-
-export { default as Topbar } from "./Topbar/Topbar"
-export { default as Sidebar } from "./Sidebar/Sidebar"
