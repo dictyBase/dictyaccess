85c993aa09293a0016d57f96bf442856d3333e03
wildlifehexagon <crossthebreeze@gmail.com>
2018-04-12 10:55:53 -0500

    reconfigure sidebar and split into appropriate components

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -1,34 +1,94 @@
 import React, { Component } from "react"
 import { BrowserRouter as Router } from "react-router-dom"
+import classNames from "classnames"
 import { Header, Footer } from "dicty-components-header-footer"
 import { Navbar } from "dicty-components-navbar"
-import { Sidebar } from "features"
+import Routes from "app/routes/Routes"
 import { FooterLinks } from "common/constants/Footer"
 import { NavbarLinks } from "common/constants/Navbar"
-// import Routes from "app/routes/Routes"
 import { headerItems, generateLinks } from "common/utils/headerItems"
-import { Container } from "./AppStyles"
+import { withStyles } from "material-ui/styles"
+import { drawerWidth } from "common/constants/Styling"
+import Topbar from "features/Topbar/Topbar"
+import Sidebar from "features/Sidebar/Sidebar"
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  appFrame: {
+    height: "100%",
+    zIndex: 1,
+    overflow: "hidden",
+    position: "relative",
+    display: "flex",
+    width: "100%"
+  },
+  drawerHeader: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: "0 8px",
+    ...theme.mixins.toolbar
+  },
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing.unit * 3,
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    marginLeft: -drawerWidth
+  },
+  contentShift: {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen
+    }),
+    marginLeft: 0
+  }
+})
 
 class App extends Component {
+  state = {
+    open: false
+  }
+
+  handleDrawerOpen = () => {
+    this.setState({ open: true })
+  }
+
+  handleDrawerClose = () => {
+    this.setState({ open: false })
+  }
+
   render() {
+    const { classes } = this.props
+    const { open } = this.state
     return (
-      <div>
+      <div className={classes.root}>
         <Router>
           <Header items={headerItems}>
             {headerItems => headerItems.map(generateLinks)}
           </Header>
         </Router>
         <Navbar items={NavbarLinks} />
-        <Container>
-          {/* <Topbar handleSideBar={this.handleSideBar} styles={styles.topbar} /> */}
-          <Sidebar />
-          {/* <div style={styles.dashboard}>{this.props.children}</div>
-          <Routes /> */}
-        </Container>
+        <div className={classes.appFrame}>
+          <Topbar open={open} handleDrawerOpen={this.handleDrawerOpen} />
+          <Sidebar open={open} handleDrawerClose={this.handleDrawerClose} />
+          <main
+            className={classNames(classes.content, {
+              [classes.contentShift]: open
+            })}>
+            <div className={classes.drawerHeader} />
+            <Routes />
+          </main>
+        </div>
         <Footer items={FooterLinks} />
       </div>
     )
   }
 }
 
-export default App
+export default withStyles(styles)(App)
--- src/features/Sidebar/Sidebar.js
+++ src/features/Sidebar/Sidebar.js
@@ -1,55 +1,15 @@
 // @flow
 import React from "react"
 import { withStyles } from "material-ui/styles"
-import classNames from "classnames"
 import Drawer from "material-ui/Drawer"
-import AppBar from "material-ui/AppBar"
-import Toolbar from "material-ui/Toolbar"
 import List, { ListItem, ListItemIcon, ListItemText } from "material-ui/List"
 import InboxIcon from "@material-ui/icons/Inbox"
 import DraftsIcon from "@material-ui/icons/Drafts"
-import Typography from "material-ui/Typography"
 import IconButton from "material-ui/IconButton"
-import MenuIcon from "@material-ui/icons/Menu"
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft"
-import Dashboard from "app/layout/Dashboard"
-
-const drawerWidth = 240
+import { drawerWidth } from "common/constants/Styling"
 
 const styles = theme => ({
-  root: {
-    flexGrow: 1
-  },
-  appFrame: {
-    height: "100%",
-    zIndex: 1,
-    overflow: "hidden",
-    position: "relative",
-    display: "flex",
-    width: "100%"
-  },
-  appBar: {
-    position: "absolute",
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    })
-  },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    }),
-    marginLeft: drawerWidth
-  },
-  menuButton: {
-    marginLeft: 12,
-    marginRight: 20
-  },
-  hide: {
-    display: "none"
-  },
   drawerPaper: {
     position: "relative",
     width: drawerWidth,
@@ -61,114 +21,47 @@ const styles = theme => ({
     justifyContent: "flex-end",
     padding: "0 8px",
     ...theme.mixins.toolbar
-  },
-  content: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.default,
-    padding: theme.spacing.unit * 3,
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    }),
-    marginLeft: -drawerWidth
-  },
-  contentShift: {
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    }),
-    marginLeft: 0
   }
 })
 
-class Sidebar extends React.Component {
-  state = {
-    open: false
-  }
+const Sidebar = props => {
+  const { classes, open, handleDrawerClose } = props
 
-  handleDrawerOpen = () => {
-    this.setState({ open: true })
-  }
-
-  handleDrawerClose = () => {
-    this.setState({ open: false })
-  }
-
-  render() {
-    const { classes } = this.props
-    const { open } = this.state
-
-    const drawer = (
-      <Drawer
-        variant="persistent"
-        anchor="left"
-        open={open}
-        classes={{
-          paper: classes.drawerPaper
-        }}>
-        <div className={classes.drawerHeader}>
-          <IconButton onClick={this.handleDrawerClose}>
-            <ChevronLeftIcon />
-          </IconButton>
-        </div>
-        <List>
-          <ListItem button>
-            <ListItemIcon>
-              <InboxIcon />
-            </ListItemIcon>
-            <ListItemText primary="Dicty Stock Center" />
-          </ListItem>
-          <ListItem button>
-            <ListItemIcon>
-              <DraftsIcon />
-            </ListItemIcon>
-            <ListItemText primary="Genomepage" />
-          </ListItem>
-          <ListItem button>
-            <ListItemIcon>
-              <DraftsIcon />
-            </ListItemIcon>
-            <ListItemText primary="Dicty Frontpage" />
-          </ListItem>
-        </List>
-      </Drawer>
-    )
-
-    return (
-      <div className={classes.root}>
-        <div className={classes.appFrame}>
-          <AppBar
-            className={classNames(classes.appBar, {
-              [classes.appBarShift]: open
-            })}>
-            <Toolbar disableGutters={!open}>
-              <IconButton
-                color="inherit"
-                aria-label="open drawer"
-                onClick={this.handleDrawerOpen}
-                className={classNames(
-                  classes.menuButton,
-                  open && classes.hide
-                )}>
-                <MenuIcon />
-              </IconButton>
-              <Typography variant="title" color="inherit" noWrap>
-                DictyAccess
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          {drawer}
-          <main
-            className={classNames(classes.content, {
-              [classes.contentShift]: open
-            })}>
-            <div className={classes.drawerHeader} />
-            <Dashboard />
-          </main>
-        </div>
+  return (
+    <Drawer
+      variant="persistent"
+      anchor="left"
+      open={open}
+      classes={{
+        paper: classes.drawerPaper
+      }}>
+      <div className={classes.drawerHeader}>
+        <IconButton onClick={handleDrawerClose}>
+          <ChevronLeftIcon />
+        </IconButton>
       </div>
-    )
-  }
+      <List>
+        <ListItem button>
+          <ListItemIcon>
+            <InboxIcon />
+          </ListItemIcon>
+          <ListItemText primary="Dicty Stock Center" />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <DraftsIcon />
+          </ListItemIcon>
+          <ListItemText primary="Genomepage" />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <DraftsIcon />
+          </ListItemIcon>
+          <ListItemText primary="Dicty Frontpage" />
+        </ListItem>
+      </List>
+    </Drawer>
+  )
 }
 
 export default withStyles(styles)(Sidebar)
