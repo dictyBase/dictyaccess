f6ab742c32f89382210d5eaa67f4b249140f271b
wildlifehexagon <crossthebreeze@gmail.com>
2018-04-13 16:44:57 -0500

    update error page

--- src/features/PageNotFound.js
+++ src/features/PageNotFound.js
@@ -1,7 +1,25 @@
 import React from "react"
+import { Link } from "react-router-dom"
+import Paper from "material-ui/Paper"
+import Button from "material-ui/Button"
+import styled from "styled-components"
+
+const PaperStyle = styled(Paper)`
+  text-align: center;
+  height: 400px;
+  padding-top: 50px;
+`
 
 const PageNotFound = () => {
-  return <div>Page Not Found (better error message coming soon)</div>
+  return (
+    <PaperStyle>
+      <h1>Oops...</h1>
+      <h3>This page does not exist.</h3>
+      <Button color="primary">
+        <Link to="/">Back to Dashboard</Link>
+      </Button>
+    </PaperStyle>
+  )
 }
 
 export default PageNotFound
