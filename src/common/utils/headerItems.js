34c222856dedef0f8d73fb5a94855f6d6c532602
wildlifehexagon <crossthebreeze@gmail.com>
2018-07-11 12:03:10 -0500

    update header-footer to newest version

--- src/common/utils/headerItems.js
+++ src/common/utils/headerItems.js
@@ -3,17 +3,31 @@ import { HeaderLink } from "dicty-components-header-footer"
 import { Link } from "react-router-dom"
 import React from "react"
 import FontAwesome from "react-fontawesome"
+import styled from "styled-components"
+import "font-awesome/css/font-awesome.min.css"
+
+const RouterLink = styled(Link)`
+  color: #15317e;
+  padding: 15px;
+  text-decoration: none;
+`
 
 const generateLinks = (link: Object, i: string) => {
   return link.isRouter ? (
-    <Link key={i} to={link.url}>
-      <FontAwesome name={link.icon} />&nbsp;
-      {link.text}
-    </Link>
+    <RouterLink key={i} to={link.url}>
+      <center>
+        <FontAwesome name={link.icon} size="2x" />
+        <br />
+        {link.text}
+      </center>
+    </RouterLink>
   ) : (
     <HeaderLink key={i} href={link.url}>
-      <FontAwesome name={link.icon} />&nbsp;
-      {link.text}
+      <center>
+        <FontAwesome name={link.icon} size="2x" />
+        <br />
+        {link.text}
+      </center>
     </HeaderLink>
   )
 }
@@ -22,48 +36,48 @@ const headerItems = [
   {
     url: "/cite",
     icon: "plus",
-    text: "Cite Us"
+    text: "Cite Us",
   },
   {
     url: "/downloads",
     icon: "download",
-    text: "Downloads"
+    text: "Downloads",
   },
   {
     url: "/about",
     icon: "info-circle",
-    text: "About dictyBase"
+    text: "About dictyBase",
   },
   {
     url: "/login",
     icon: "sign-in",
     text: "Login",
-    isRouter: true
-  }
+    isRouter: true,
+  },
 ]
 
 const loggedHeaderItems = [
   {
     url: "/cite",
     icon: "plus",
-    text: "Cite Us"
+    text: "Cite Us",
   },
   {
     url: "/downloads",
     icon: "download",
-    text: "Downloads"
+    text: "Downloads",
   },
   {
     url: "/about",
     icon: "info-circle",
-    text: "About dictyBase"
+    text: "About dictyBase",
   },
   {
     url: "/logout",
     icon: "sign-out",
     text: "Logout",
-    isRouter: true
-  }
+    isRouter: true,
+  },
 ]
 
 export { headerItems, loggedHeaderItems, generateLinks }
