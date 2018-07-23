e99b7307b5cfb200cacb9ffe4b2809940e046ce6
wildlifehexagon <crossthebreeze@gmail.com>
2018-07-23 14:48:10 -0500

    add react-ga and tracking id

--- src/index.js
+++ src/index.js
@@ -22,6 +22,24 @@ jss.options.insertionPoint = document.getElementById("jss-insertion-point")
 
 const store = configureStore()
 
+const setGoogleAnalytics = async (location, action) => {
+  try {
+    const module = await import("react-ga")
+    let ReactGA = module.default
+    ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID)
+    ReactGA.set({ page: window.location.pathname })
+    ReactGA.pageview(window.location.pathname)
+  } catch (e) {
+    console.error("could not load react-ga module", JSON.stringify(e))
+  }
+}
+
+if (process.env.NODE_ENV === "production") {
+  history.listen((location, action) => {
+    setGoogleAnalytics(location, action)
+  })
+}
+
 ReactDOM.render(
   <JssProvider jss={jss} generateClassName={generateClassName}>
     <Provider store={store}>
