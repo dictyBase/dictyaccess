d783863f9b46f2d9be28a2c05cc5e94a8a152fbb
wildlifehexagon <crossthebreeze@gmail.com>
2018-10-10 09:40:13 -0500

    fix: update login redirect path to use basename
    fix #23

--- src/features/Authentication/AuthLoader.js
+++ src/features/Authentication/AuthLoader.js
@@ -1,22 +1,22 @@
 // @flow
 import React from "react"
 import Grid from "@material-ui/core/Grid"
-import FontAwesome from "react-fontawesome"
+import CircularProgress from "@material-ui/core/CircularProgress"
 
 /**
  * Loading screen during the login process
  */
 
-const AuthLoader = () => {
-  return (
-    <Grid container justify="center">
+const AuthLoader = () => (
+  <Grid container justify="center">
+    <Grid item xs={12}>
       <center>
         <h1>Logging in...</h1>
         <br />
-        <FontAwesome name="spinner" size="5x" pulse fixedWidth />
+        <CircularProgress size={70} />
       </center>
     </Grid>
-  )
-}
+  </Grid>
+)
 
 export default AuthLoader
