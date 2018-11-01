d7b625a32fb25bb0ebebe56b55bec5bb2904fa42
wildlifehexagon <crossthebreeze@gmail.com>
2018-11-01 15:47:28 -0500

    fix: add padding to bird's eye dashboard

--- src/common/components/TypographyWrapper.js
+++ src/common/components/TypographyWrapper.js
@@ -3,7 +3,9 @@ import React from "react"
 import Typography from "@material-ui/core/Typography"
 
 const TypographyWrapper = ({ children }: any) => (
-  <Typography component="div">{children}</Typography>
+  <Typography component="div" style={{ padding: 8 * 3 }}>
+    {children}
+  </Typography>
 )
 
 export default TypographyWrapper
