54d28fee5c95ee0c2a02c2ce834d74e38c6a889f
wildlifehexagon <crossthebreeze@gmail.com>
2019-07-29 12:50:52 -0500

    build: add helm upgrade command and dockerignore file

--- docker/eric-dev/local-ci.sh
+++ docker/eric-dev/local-ci.sh
@@ -2,3 +2,4 @@
 tag=$(git symbolic-ref -q HEAD | sed -e 's|^refs/heads/||' | sed 's/\//-/')-$(git rev-parse --short HEAD)
 docker build --rm -f docker/eric-dev/Dockerfile -t dictybase/dictyaccess:${tag} .
 docker push dictybase/dictyaccess:${tag}
+helm upgrade dictyaccess dictybase/dictyaccess --namespace dictybase --set image.tag=${tag}
