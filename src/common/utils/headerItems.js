cc5990fc728ec9718eb7346fbf4d9f64bd936c5d
wildlifehexagon <crossthebreeze@gmail.com>
2019-07-30 16:03:41 -0500

    fix: remove css line, add classnames

--- src/common/utils/headerItems.js
+++ src/common/utils/headerItems.js
@@ -3,7 +3,6 @@ import { HeaderLink } from "dicty-components-header-footer"
 import { Link } from "react-router-dom"
 import React from "react"
 import FontAwesome from "react-fontawesome"
-import "font-awesome/css/font-awesome.min.css"
 
 const generateLinks = (link: Object, i: string) =>
   link.isRouter ? (
