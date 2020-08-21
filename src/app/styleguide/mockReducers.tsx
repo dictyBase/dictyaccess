afa758996e672beacc8eb2f87c4eef34e65c3d4a
wildlifehexagon <crossthebreeze@gmail.com>
2020-08-21 16:15:08 -0500

    fix: add type for mock reducer

--- src/app/styleguide/mockReducers.tsx
+++ src/app/styleguide/mockReducers.tsx
@@ -1,7 +1,7 @@
 import { combineReducers } from "redux"
 import birdsEyeReducer from "../reducers/birdsEyeReducer"
 
-export default (history) =>
+export default (history: any) =>
   combineReducers({
     birdseye: birdsEyeReducer,
   })
