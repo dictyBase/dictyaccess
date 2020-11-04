a670dea99ad8015bf57e82a289f620c5d38fef7e
wildlifehexagon <crossthebreeze@gmail.com>
2020-11-04 14:15:34 -0600

    refactor: update apollo imports

--- src/app/styleguide/Wrapper.tsx
+++ src/app/styleguide/Wrapper.tsx
@@ -1,33 +1,26 @@
 import React from "react"
 import { Provider } from "react-redux"
 import { BrowserRouter } from "react-router-dom"
-import { ApolloProvider } from "@apollo/react-hooks"
-import { ApolloClient } from "apollo-client"
-import { InMemoryCache } from "apollo-cache-inmemory"
-import { createHttpLink } from "apollo-link-http"
+import { ApolloProvider } from "@apollo/client"
 import { createStore } from "redux"
+import useApolloClient from "common/hooks/useApolloClient"
 import rootReducer from "./mockReducers"
 
 const store = createStore(rootReducer)
-
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: createHttpLink({
-    uri: `${process.env.REACT_APP_GRAPHQL_SERVER}/graphql`,
-    credentials: "include",
-  }),
-})
-
 /**
  * This is a wrapper component used for all styleguidist documentation.
  */
 
-const Wrapper = ({ children }: any) => (
-  <ApolloProvider client={client}>
-    <Provider store={store}>
-      <BrowserRouter>{children}</BrowserRouter>
-    </Provider>
-  </ApolloProvider>
-)
+const Wrapper = ({ children }: { children: React.ReactNode }) => {
+  const client = useApolloClient()
+
+  return (
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <BrowserRouter>{children}</BrowserRouter>
+      </Provider>
+    </ApolloProvider>
+  )
+}
 
 export default Wrapper
--- src/common/graphql/query.ts
+++ src/common/graphql/query.ts
@@ -1,4 +1,4 @@
-import gql from "graphql-tag"
+import { gql } from "@apollo/client"
 
 const GET_REFRESH_TOKEN = gql`
   query GetRefreshToken($token: String!) {
