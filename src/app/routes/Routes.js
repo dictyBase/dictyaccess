3fdef22c32301cbc957e949f9f7e388ec0c9a063
wildlifehexagon <hartline.eric@gmail.com>
2021-05-05 11:33:22 -0500

    refactor: use custom hook for google analytics

--- src/app/routes/Routes.js
+++ src/app/routes/Routes.js
@@ -16,53 +16,64 @@ import PageNotFound from "features/PageNotFound"
 import BirdsEyeComparative from "features/BirdsEye/BirdsEyeComparative"
 import BirdsEyeSpatialExpression from "features/BirdsEye/BirdsEyeSpatialExpression"
 import CircosSeqContainer from "features/BirdsEye/Circos/CircosSeqContainer"
+import useGoogleAnalytics from "common/hooks/useGoogleAnalytics"
 
 /**
  * List of routes used with React Router.
  */
 
-const Routes = () => (
-  <Switch>
-    <Route
-      exact
-      path="/"
-      component={(BirdsEyeDashboard) => <Redirect to="/birdseye/genes" />}
-    />
-    <Route
-      exact
-      path="/birdseye"
-      component={(BirdsEyeDashboard) => <Redirect to="/birdseye/genes" />}
-    />
-    <Route exact path="/birdseye/genes" component={BirdsEyeDashboard} />
-    <Route
-      exact
-      path="/birdseye/genes/:id"
-      render={({ location }) => (
-        <CircosGenesContainer key={location.pathname} />
-      )}
-    />
-    <Route exact path="/birdseye/comparative" component={BirdsEyeComparative} />
-    <Route
-      exact
-      path="/birdseye/spatial-expression"
-      component={BirdsEyeSpatialExpression}
-    />
-    <Route
-      exact
-      path="/birdseye/spatial-expression/:id"
-      render={({ location }) => <CircosSeqContainer key={location.pathname} />}
-    />
-    <Route exact path="/genomes" component={GenomeDashboard} />
-    <Route exact path="/annotations" component={AnnotationsDashboard} />
-    <Route exact path="/analytics" component={AnalyticsDashboard} />
-    <Route exact path="/stockcenter" component={DscDashboard} />
-    <Route exact path="/phylogeny" component={PhylogenyDashboard} />
-    <Route exact path="/login" component={Login} />
-    <Route exact path="/:provider/callback" component={OauthCallback} />
-    <Route exact path="/load/auth" component={AuthLoader} />
-    <Route exact path="/logout" component={Logout} />
-    <Route path="*" component={PageNotFound} />
-  </Switch>
-)
+const Routes = () => {
+  useGoogleAnalytics()
+
+  return (
+    <Switch>
+      <Route
+        exact
+        path="/"
+        component={(BirdsEyeDashboard) => <Redirect to="/birdseye/genes" />}
+      />
+      <Route
+        exact
+        path="/birdseye"
+        component={(BirdsEyeDashboard) => <Redirect to="/birdseye/genes" />}
+      />
+      <Route exact path="/birdseye/genes" component={BirdsEyeDashboard} />
+      <Route
+        exact
+        path="/birdseye/genes/:id"
+        render={({ location }) => (
+          <CircosGenesContainer key={location.pathname} />
+        )}
+      />
+      <Route
+        exact
+        path="/birdseye/comparative"
+        component={BirdsEyeComparative}
+      />
+      <Route
+        exact
+        path="/birdseye/spatial-expression"
+        component={BirdsEyeSpatialExpression}
+      />
+      <Route
+        exact
+        path="/birdseye/spatial-expression/:id"
+        render={({ location }) => (
+          <CircosSeqContainer key={location.pathname} />
+        )}
+      />
+      <Route exact path="/genomes" component={GenomeDashboard} />
+      <Route exact path="/annotations" component={AnnotationsDashboard} />
+      <Route exact path="/analytics" component={AnalyticsDashboard} />
+      <Route exact path="/stockcenter" component={DscDashboard} />
+      <Route exact path="/phylogeny" component={PhylogenyDashboard} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/:provider/callback" component={OauthCallback} />
+      <Route exact path="/load/auth" component={AuthLoader} />
+      <Route exact path="/logout" component={Logout} />
+      <Route path="*" component={PageNotFound} />
+    </Switch>
+  )
+}
 
 export default Routes
